792e31397a920ff099112691d160802b9d390eca
Raúl Bermúdez González <raul.bermudez@atmira.com>
2025-04-23 13:18:40 +0200

    Creo el controlador y el servicio para recoger todos los asteroides

--- src/main/java/com/example/apiNasa/controller/AsteroideController.java
+++ src/main/java/com/example/apiNasa/controller/AsteroideController.java
@@ -1,4 +1,34 @@
 package com.example.apiNasa.controller;
 
+import com.example.apiNasa.dto.AsteroidResponse;
+import com.example.apiNasa.service.AsteroideService;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Validated
 public class AsteroideController {
+    @Autowired
+    private AsteroideService asteroidService;
+
+    @GetMapping("/asteroids")
+    public ResponseEntity<?> obtenerAsteroides(@RequestParam(required = false) Integer days) {
+        if (days == null) {
+            return ResponseEntity.badRequest().body("El parámetro 'days' es obligatorio.");
+        }
+
+        if (days < 1 || days > 7) {
+            return ResponseEntity.badRequest().body("El parámetro 'days' debe estar entre 1 y 7.");
+        }
+
+        JsonNode topAsteroids = asteroidService.obtenerAsteroidesPotenciales(days);
+        return ResponseEntity.ok(topAsteroids);
+    }
 }
--- src/main/java/com/example/apiNasa/service/AsteroideService.java
+++ src/main/java/com/example/apiNasa/service/AsteroideService.java
@@ -1,4 +1,38 @@
 package com.example.apiNasa.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+
+@Service
 public class AsteroideService {
+
+    private static final String NASA_API_KEY = "DEMO_KEY";
+    private static final String NASA_API_URL = "https://api.nasa.gov/neo/rest/v1/feed";
+
+    public JsonNode obtenerAsteroidesPotenciales(int days) {
+        RestTemplate restTemplate = new RestTemplate();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // Para tener el dia en el que estamos
+        LocalDate fecha_inicio = LocalDate.now();
+        // Para sumar dias, tambien se puede sumar directamente meses y años
+        LocalDate fecha_final = fecha_inicio.plusDays(days);
+
+        String url = NASA_API_URL + "?start_date=" + fecha_inicio + "&end_date=" + fecha_final + "&api_key=" + NASA_API_KEY;
+
+        JsonNode asteroides = null;
+
+        try {
+            String response = restTemplate.getForObject(url, String.class);
+            asteroides = objectMapper.readTree(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return asteroides;
+    }
 }
