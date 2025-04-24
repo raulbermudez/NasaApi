a21e7de345c71c265884d27522854c87cfc8fa98
Raúl Bermúdez González <raul.bermudez@atmira.com>
2025-04-24 10:18:28 +0200

    Elimino lineas innecesarias del controlador, hago que el servicio me de solo los datos que necesitamos de los asteroides

--- src/main/java/com/example/apiNasa/controller/AsteroideController.java
+++ src/main/java/com/example/apiNasa/controller/AsteroideController.java
@@ -1,11 +1,9 @@
 package com.example.apiNasa.controller;
 
-import com.example.apiNasa.dto.AsteroidResponse;
 import com.example.apiNasa.service.AsteroideService;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@Validated
 public class AsteroideController {
     @Autowired
     private AsteroideService asteroidService;
--- src/main/java/com/example/apiNasa/service/AsteroideService.java
+++ src/main/java/com/example/apiNasa/service/AsteroideService.java
@@ -2,10 +2,14 @@ package com.example.apiNasa.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class AsteroideService {
@@ -25,14 +29,60 @@ public class AsteroideService {
         String url = NASA_API_URL + "?start_date=" + fecha_inicio + "&end_date=" + fecha_final + "&api_key=" + NASA_API_KEY;
 
         JsonNode asteroides = null;
+        // Creo el array que posteriormente devolvere
+        ArrayNode resultado = objectMapper.createArrayNode();
 
         try {
             String response = restTemplate.getForObject(url, String.class);
             asteroides = objectMapper.readTree(response);
+
+
+            // Para leer un campo específico del Json usamos el get
+            JsonNode nearEarthObjects = asteroides.path("near_earth_objects");
+
+            for (JsonNode asteroide: nearEarthObjects) {
+                for (JsonNode asteroideFinal: asteroide){
+                    // Compruebo primero que sean potencialmente peligrosos
+                    if (asteroideFinal.get("is_potentially_hazardous_asteroid").asBoolean()){
+                        // Obtenemos los nombres de los asteroides
+                        String nombre = asteroideFinal.get("name").asText();
+                        ObjectNode superAsteroide = objectMapper.createObjectNode();
+                        superAsteroide.put("nombre", nombre);
+
+                        // Obtenemos el diametro de los asteroides
+                        double diametro_max = asteroideFinal.get("estimated_diameter")
+                                                            .get("kilometers")
+                                                            .get("estimated_diameter_max").asDouble();
+                        double diametro_min = asteroideFinal.get("estimated_diameter")
+                                                            .get("kilometers")
+                                                            .get("estimated_diameter_min")
+                                                            .asDouble();
+                        double diametro_med = (diametro_max + diametro_min) / 2;
+                        superAsteroide.put("diametro", diametro_med);
+
+                        // Obtenemos la velocidad del asteroide
+                        String velocidad = asteroideFinal.get("close_approach_data").get(0).get("relative_velocity")
+                                .get("kilometers_per_hour").asText();
+                        superAsteroide.put("velocidad", velocidad);
+
+                        // Obtenemos la fecha
+                        String fecha = asteroideFinal.get("close_approach_data").get(0).get("close_approach_date").asText();
+                        superAsteroide.put("fecha", fecha);
+
+                        // Por ultimo obtenemos el planeta
+                        String planeta = asteroideFinal.get("close_approach_data").get(0).get("orbiting_body").asText();
+                        superAsteroide.put("planeta", planeta);
+
+                        resultado.add(superAsteroide);
+                    }
+                }
+            }
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return asteroides;
+        return resultado;
     }
 }
